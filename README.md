7072c6372223be66df00042103583554094f83a4
Pouriya <pouriya.jamshidi@gmail.com>
2021-02-26 16:34:39 +0330

    Init

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
-# tcping
-ping tcp ports. Inspired by Linux's ping utility. Written in Go
+# TCPING
+
+A cross-platform ping program for ```TCP``` ports similar to Linux's ping utility. This program will send ```TCP``` probes to an ```IP address``` or a ```hostname``` specified by you and prints the results.
+
+It uses different sequence numbering for successful and unsuccessful probes, so that when you look at the results after a while, and seeing for instance, a failed probe, understanding the total packet drops so far would be illustrative enough.
+
+## Application
+
+* Calculate packet loss.
+* Assess latency of your network.
+* Show min/avg/max probes latency.
+* Monitor and audit your peers network.
+* Calculate total up or downtime.
+* Make sure a host is up in environments that ping is blocked.
+* etc.
+
+## Images
+
+![WindowsVersion](/Images/windowsVersion.png)
+
+## Demo
+
+[![asciicast](https://asciinema.org/a/bNMtJKmujGEpfEhvDiTeSvtO4.svg)](https://asciinema.org/a/bNMtJKmujGEpfEhvDiTeSvtO4)
+
+## Download for
+
+* ### [Windows](https://github.com/pouriyajamshidi/tcping/releases/download/Windows-v1.0.0/tcping.exe)
+
+* ### [Linux](https://github.com/pouriyajamshidi/tcping/releases/download/Linux-v1.0.0/tcping)
+
+* ### [macOS](https://github.com/pouriyajamshidi/tcping/releases/download/macOS-v1.0.0/tcping)
+
+## Usage
+
+Go to the directory/folder in which you have downloaded the application.
+
+### On ```Linux``` and ```macOS```:
+
+```bash
+sudo chmod +x tcping
+```
+
+For easier use, you can copy it to your system ```PATH``` like /bin/ or /usr/bin/
+
+```bash
+sudo cp tcping /bin/
+```
+
+Then run it like:
+
+```bash
+tcping www.example.com 443
+```
+
+OR
+
+```bash
+tcping 10.10.10.1 22
+```
+
+### On ```Windows```
+
+I recommend ```Windows Terminal``` for the best experience and proper colorization.
+
+For easier use, copy ```tcping.exe``` to your system ```PATH``` like C:\Windows\System32 or from your terminal application, go to the folder that contains the ```tcping.exe``` program.
+
+Run it like:
+
+```powershell
+.\tcping www.example.com 443
+```
+
+OR
+
+```powershell
+tcping 10.10.10.1 22
+```
+
+**Please note, if you copy the program to your system ```PATH```, you don't need to specify ```.\``` to run the program anymore.**
+
+## Tips
+
+* While the program is running, upon pressing the ```enter``` key, the summary of all probes will be shown.
+
+## Notes
+
+This program is still in a ```beta``` stage. There are several shortcomings that I will rectify in the near future.
+
+## Tested on
+
+Windows, Linux and macOS
+
+## Contributing
+
+Pull requests are welcome.
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
---
+++ tcping.go
@@ -0,0 +1,296 @@
+/*
+					TODO
+=====================================================
+*) calc and print up/down time in a proper way
+*) Print TTL?
+*) Print src and dst port?
+*) is it not better to not use global variables?
+*) if already an IP, dont show it in print twice
+
+*/
+
+package main
+
+import (
+	"bufio"
+	"net"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+
+	"github.com/gookit/color"
+)
+
+var (
+	minTime           uint
+	aveTime           uint
+	maxTime           uint
+	totalSucPackets   uint
+	totalUnsucPackets uint
+	totalDowntime     uint
+	totalUptime       uint
+	timeArray         []uint
+	successCounter    uint = 0
+	failureCounter    uint = 0
+	wasDown           bool = false
+	downTime          time.Time
+)
+
+/* Print how program should be run */
+func usage() {
+	color.Red.Printf("Invalid input. Try running %s like:\n", os.Args[0])
+	color.Red.Printf("%s HOSTNAME/IP PORT-NUMBER... example:\n", os.Args[0])
+	color.Red.Printf("%s www.example.com 443\n", os.Args[0])
+	os.Exit(1)
+}
+
+/* Catch SIGINT and print a summary of stats */
+func signalHandler(host string) {
+	sig := make(chan os.Signal)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-sig
+		printResults(host)
+		os.Exit(0)
+	}()
+}
+
+/* Validate user input */
+func validateInput() (string, string, string) {
+	args := os.Args[1:]
+
+	if len(args) != 2 {
+		usage()
+	}
+
+	host := args[0]
+	port := args[1]
+	portInt, _ := strconv.Atoi(port)
+
+	if portInt < 1 || portInt > 65535 {
+		print("Port should be in 1..65535 range\n")
+		os.Exit(1)
+	}
+
+	IPaddr := resolveHostname(host)
+	IP := IPaddr[0].String()
+
+	return host, port, IP
+}
+
+/* Hostname resolution */
+func resolveHostname(host string) []net.IP {
+	IPaddr, err := net.LookupIP(host)
+
+	if err != nil {
+		color.Red.Printf("Failed to resolve %s\n", host)
+		os.Exit(1)
+	}
+	return IPaddr
+}
+
+/* find min/avg/max RTT values */
+func findMinAvgMaxTime(x []uint) (uint, float32, uint, int) {
+
+	var avgTime float32 = 0
+	var placeHolder uint
+
+	arrLen := len(x)
+
+	for i := 0; i < arrLen; i++ {
+		if x[i] == 0 {
+			continue
+		}
+		avgTime += float32(x[i])
+	}
+
+	if avgTime == 0 {
+		return 0, 0, 0, -1
+	}
+
+	for i := 0; i < arrLen-1; i++ {
+
+		for j := 0; j < arrLen-i-1; j++ {
+
+			if x[j] > x[j+1] {
+				placeHolder = x[j+1]
+				x[j+1] = x[j]
+				x[j] = placeHolder
+			}
+		}
+	}
+
+	return x[0], avgTime / float32(arrLen), x[arrLen-1], 0
+}
+
+/* Calculate cumulative time */
+func formatTime(time uint) string {
+	var timestr string
+
+	if time == 1 {
+		timestr = strconv.FormatUint(uint64(time), 10) + " second"
+		return timestr
+	} else if time < 60 {
+		timestr = strconv.FormatUint(uint64(time), 10) + " seconds"
+		return timestr
+	} else {
+		timeFloat := float64(time) / 60
+
+		if timeFloat < 2 {
+			timestr = strconv.FormatFloat(timeFloat, 'f', 2, 32) + " second"
+			return timestr
+		} else if timeFloat < 60 {
+			timestr = strconv.FormatFloat(timeFloat, 'f', 2, 32) + " seconds"
+			return timestr
+		}
+		timestr = strconv.FormatFloat(timeFloat, 'f', 2, 32) + " minutes"
+		return timestr
+	}
+}
+
+/* Print stattistics when program exits */
+func printResults(host string) {
+
+	totaluptime := formatTime(totalSucPackets)
+	totaldowntime := formatTime(totalUnsucPackets)
+	min, avg, max, err := findMinAvgMaxTime(timeArray)
+
+	if err == -1 {
+		return
+	}
+
+	totalPackets := totalSucPackets + totalUnsucPackets
+	packetLoss := (float32(totalUnsucPackets) / float32(totalPackets)) * 100
+
+	color.Yellow.Printf("\n--- %s TCPing statistics ---\n", host)
+
+	color.Yellow.Printf("%d probes transmitted, ", totalPackets)
+	color.Yellow.Printf("%d received, ", totalSucPackets)
+
+	if packetLoss == 0 {
+		color.Green.Printf("%.2f%%", packetLoss)
+	} else if packetLoss > 1 && packetLoss <= 30 {
+		color.LightYellow.Printf("%.2f%%", packetLoss)
+	} else {
+		color.Red.Printf("%.2f%%", packetLoss)
+	}
+	color.Yellow.Printf(" packet loss\n")
+
+	color.Yellow.Printf("successful probes:   ")
+	color.Green.Printf("%d\n", totalSucPackets)
+	color.Yellow.Printf("unsuccessful probes: ")
+	color.Red.Printf("%d\n", totalUnsucPackets)
+
+	color.Yellow.Printf("total uptime: ")
+	color.Green.Printf("  %s\n", totaluptime)
+	color.Yellow.Printf("total downtime: ")
+	color.Red.Printf("%s\n", totaldowntime)
+
+	/* otherwise will be buggy on Windows */
+	color.Yellow.Printf("rtt ")
+	color.Green.Printf("min")
+	color.Yellow.Printf("/")
+	color.Cyan.Printf("avg")
+	color.Yellow.Printf("/")
+	color.Red.Printf("max: ")
+	color.Green.Printf("%d", min)
+	color.Yellow.Printf("/")
+	color.Cyan.Printf("%.2f", avg)
+	color.Yellow.Printf("/")
+	color.Red.Printf("%d", max)
+	color.Yellow.Printf(" ms\n")
+}
+
+/* Ping host, TCP style */
+func tcping(host string, port string, IP string) {
+
+	hostAndPort := IP + ":" + port
+
+	startTime := time.Now()
+
+	conn, err := net.DialTimeout("tcp", hostAndPort, 1*time.Second)
+
+	timeDiff := time.Since(startTime)
+	timeDiffInMilSec := timeDiff.Milliseconds()
+
+	if err != nil {
+
+		if !wasDown {
+			downTime = time.Now()
+			wasDown = true
+		}
+
+		downtime := uint(time.Since(startTime) / time.Second)
+		totalDowntime += downtime
+
+		failureCounter++
+		totalUnsucPackets++
+
+		color.Red.Printf("No reply from %s (%s) on port %s TCP_seq=%d\n",
+			host, IP, port, failureCounter)
+
+	} else {
+
+		if wasDown {
+
+			downtime := uint(time.Since(downTime) / time.Second)
+			totalDowntime += downtime
+
+			if downtime == 1 {
+				color.Yellow.Printf("No response received for %d second\n", downtime)
+			} else if downtime < 60 {
+				color.Yellow.Printf("No response received for %d seconds\n", downtime)
+			} else {
+				color.Yellow.Printf("No response received for %d minutes\n", downtime/60)
+			}
+
+			wasDown = false
+		}
+
+		timeArray = append(timeArray, uint(timeDiffInMilSec))
+
+		successCounter++
+		totalSucPackets++
+
+		color.LightGreen.Printf("Reply from %s (%s) on port %s TCP_seq=%d time=%d ms\n",
+			host, IP, port, successCounter, timeDiff.Milliseconds())
+
+		conn.Close()
+	}
+	time.Sleep(1 * time.Second)
+}
+
+func main() {
+
+	host, port, IP := validateInput()
+
+	signalHandler(host)
+
+	color.LightCyan.Printf("TCPinging %s on port %s\n", host, port)
+
+	channel := make(chan string)
+
+	go func(channel chan string) {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			key, _ := reader.ReadString('\n')
+			channel <- key
+		}
+	}(channel)
+
+	for {
+		tcping(host, port, IP)
+
+		select {
+		case stdin, _ := <-channel:
+			if stdin == "\n" || stdin == "\r" || stdin == "\r\n" {
+				printResults(host)
+			}
+		default:
+			continue
+		}
+	}
+}
