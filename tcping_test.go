f67cc6ce4afc377f9a6ede4266d57552d23413a0
Pouriya <pouriya.jamshidi@gmail.com>
2023-08-05 15:32:05 +0200

    - rework hostname resolution logic - replace net.IP with netip.Addr - update tests

--- tcping_test.go
+++ tcping_test.go
@@ -154,3 +154,52 @@ func TestNanoToMilliseconds(t *testing.T) {
 		})
 	}
 }
+
+func TestSelectResolvedIPv4(t *testing.T) {
+	userInputV4 := userInput{
+		useIPv4: true,
+	}
+
+	stats := createTestStats(t)
+	stats.userInput = userInputV4
+
+	var (
+		ip1 = netip.MustParseAddr("172.20.10.238")
+		ip2 = netip.MustParseAddr("8.8.8.8")
+	)
+
+	t.Run("IPv4 Selection", func(t *testing.T) {
+		actual := selectResolvedIP(stats, []netip.Addr{ip1, ip2})
+
+		if !actual.IsValid() {
+			t.Errorf("Expected an IP but got invalid address")
+		}
+		if actual != ip1 && actual != ip2 {
+			t.Errorf("Expected an IP but got invalid address")
+		}
+	})
+}
+
+func TestSelectResolvedIPv6(t *testing.T) {
+	userInputV6 := userInput{
+		useIPv6: true,
+	}
+
+	stats := createTestStats(t)
+	stats.userInput = userInputV6
+
+	var (
+		ip1 = netip.MustParseAddr("2001:0db8:85a3:0000:0000:8a2e:0370:7334")
+		ip2 = netip.MustParseAddr("2001:4860:4860::8888")
+	)
+
+	t.Run("IPv6 Selection", func(t *testing.T) {
+		actual := selectResolvedIP(stats, []netip.Addr{ip1, ip2})
+		if !actual.IsValid() {
+			t.Errorf("Expected an IP but got invalid address")
+		}
+		if actual != ip1 && actual != ip2 {
+			t.Errorf("Expected an IP but got invalid address")
+		}
+	})
+}
