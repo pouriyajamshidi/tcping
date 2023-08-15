78d8b541555e056a0b7a604660ead4faf1cc9692
sakib <sakibul706@gmail.com>
2023-08-15 10:15:58 +0600

    rewritten

--- db.go
+++ db.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"math"
 	"strings"
 	"time"
 	"unicode"
@@ -16,51 +17,49 @@ const (
 	tableSchema = `
 CREATE TABLE %s (
     id INTEGER PRIMARY KEY,
-    type TEXT NOT NULL,
-    message TEXT,
+    type TEXT NOT NULL, -- for the data type
     timestamp DATETIME NOT NULL,
-    ip TEXT,
+
+    addr TEXT,
     hostname TEXT,
-    hostname_resolve_tries INTEGER,
-    hostname_changes TEXT,
-    is_ip INTEGER,
     port INTEGER,
-    success INTEGER,
-    latency REAL,
+    hostname_resolve_tries INTEGER,
+
+    hostname_changed_to TEXT,
+    hostname_change_time DATETIME,
+
     latency_min REAL,
     latency_avg REAL,
     latency_max REAL,
+
 	total_duration TEXT,
     start_timestamp DATETIME,
     end_timestamp DATETIME,
+
     last_successful_probe DATETIME,
     last_unsuccessful_probe DATETIME,
+
     longest_uptime TEXT,
     longest_uptime_end DATETIME,
     longest_uptime_start DATETIME,
+
     longest_downtime TEXT,
     longest_downtime_start DATETIME,
     longest_downtime_end DATETIME,
-    total_packet_loss REAL,
+
     total_packets INTEGER,
+    total_packet_loss REAL,
     total_successful_probes INTEGER,
     total_unsuccessful_probes INTEGER,
+
     total_uptime TEXT,
     total_downtime TEXT
-);
-
--- Host name change table
-CREATE TABLE %s (
-    id INTEGER PRIMARY KEY,
-    ip TEXT,
-    time DATETIME
 );`
 )
 
 type saveDb struct {
-	db                  *sql.DB
-	tableName           string
-	hostnameChangeTable string
+	db        *sql.DB
+	tableName string
 }
 
 func (s saveDb) saveToDb(query string, arg ...any) {
@@ -71,15 +70,15 @@ func (s saveDb) saveToDb(query string, arg ...any) {
 }
 
 func (s saveDb) saveHostNameChange(h hostnameChange) {
-	statement := fmt.Sprintf("INSERT INTO %s (ip, time) VALUES (?, ?)", s.hostnameChangeTable)
+	statement := fmt.Sprintf("INSERT INTO %s (ip, time) VALUES (?, ?)", s.tableName)
 	if _, err := s.db.Exec(statement, h.Addr.String(), h.When); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func newDb(args []string, dbPath string) saveDb {
-	tableName, hostnameChangeTable := newTableNames(args)
-	tableSchema := fmt.Sprintf(tableSchema, tableName, hostnameChangeTable)
+	tableName := newTableNames(args)
+	tableSchema := fmt.Sprintf(tableSchema, tableName)
 
 	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
@@ -91,10 +90,10 @@ func newDb(args []string, dbPath string) saveDb {
 		log.Fatal(err)
 	}
 
-	return saveDb{db, tableName, hostnameChangeTable}
+	return saveDb{db, tableName}
 }
 
-func newTableNames(args []string) (string, string) {
+func newTableNames(args []string) string {
 	if len(args) < 2 {
 		usage()
 	}
@@ -103,97 +102,106 @@ func newTableNames(args []string) (string, string) {
 	if unicode.IsNumber(rune(tableName[0])) {
 		tableName = "_" + tableName
 	}
-	hostnameChangeTable := fmt.Sprintf("hostname_changes_%s", strings.TrimPrefix(tableName, "_"))
 
-	return tableName, hostnameChangeTable
+	return tableName
 }
 
 func (s saveDb) printStart(hostname string, port uint16) {
-	s.saveToDb("INSERT INTO %s (type, hostname, port, timestamp) VALUES (?, ?, ?, ?)", startEvent, hostname, port, time.Now())
+	// s.saveToDb("INSERT INTO %s (type, hostname, port, timestamp) VALUES (?, ?, ?, ?)", startEvent, hostname, port, time.Now())
 }
 
 func (s saveDb) printProbeSuccess(hostname, ip string, port uint16, streak uint, rtt float32) {
-	success := 1 // true
-	if hostname != "" {
-		isIp := 0
-		statement := "INSERT INTO %s (type, hostname, ip, port, is_ip, success, total_successful_probes, latency, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
-		s.saveToDb(statement, probeEvent, hostname, ip, port, isIp, success, streak, rtt, time.Now())
-	} else {
-		isIp := 1
-		statement := "INSERT INTO %s (type, ip, port, is_ip, success, total_successful_probes, latency, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
-		s.saveToDb(statement, probeEvent, ip, port, isIp, success, streak, rtt, time.Now())
-	}
+	// success := 1 // true
+	// if hostname != "" {
+	// 	isIp := 0
+	// 	statement := "INSERT INTO %s (type, hostname, ip, port, is_ip, success, total_successful_probes, latency, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+	// 	s.saveToDb(statement, probeEvent, hostname, ip, port, isIp, success, streak, rtt, time.Now())
+	// } else {
+	// 	isIp := 1
+	// 	statement := "INSERT INTO %s (type, ip, port, is_ip, success, total_successful_probes, latency, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+	// 	s.saveToDb(statement, probeEvent, ip, port, isIp, success, streak, rtt, time.Now())
+	// }
 }
 
 func (s saveDb) printProbeFail(hostname, ip string, port uint16, streak uint) {
-	success := 0 // false
-	if hostname != "" {
-		isIp := 0
-		statement := "INSERT INTO %s (type, hostname, ip, port, is_ip, success, total_unsuccessful_probes, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
-		s.saveToDb(statement, probeEvent, hostname, ip, port, isIp, success, streak, time.Now())
-	} else {
-		isIp := 1
-		statement := "INSERT INTO %s (type, ip, port, is_ip, success, total_unsuccessful_probes, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?)"
-		s.saveToDb(statement, probeEvent, ip, port, isIp, success, streak, time.Now())
-	}
+	// success := 0 // false
+	// if hostname != "" {
+	// 	isIp := 0
+	// 	statement := "INSERT INTO %s (type, hostname, ip, port, is_ip, success, total_unsuccessful_probes, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+	// 	s.saveToDb(statement, probeEvent, hostname, ip, port, isIp, success, streak, time.Now())
+	// } else {
+	// 	isIp := 1
+	// 	statement := "INSERT INTO %s (type, ip, port, is_ip, success, total_unsuccessful_probes, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?)"
+	// 	s.saveToDb(statement, probeEvent, ip, port, isIp, success, streak, time.Now())
+	// }
 }
 
 func (s saveDb) printRetryingToResolve(hostname string) {
-	statement := "INSERT INTO %s (type, hostname, timestamp) VALUES (?, ?, ?)"
-	s.saveToDb(statement, retryEvent, hostname, time.Now())
+	// statement := "INSERT INTO %s (type, hostname, timestamp) VALUES (?, ?, ?)"
+	// s.saveToDb(statement, retryEvent, hostname, time.Now())
 }
 func (s saveDb) printTotalDownTime(downtime time.Duration) {
-	statement := "INSERT INTO %s (type, total_downtime, timestamp) VALUES (?, ?, ?)"
-	s.saveToDb(statement, retrySuccessEvent, downtime.Seconds(), time.Now())
+	// statement := "INSERT INTO %s (type, total_downtime, timestamp) VALUES (?, ?, ?)"
+	// s.saveToDb(statement, retrySuccessEvent, downtime.Seconds(), time.Now())
 }
 
-func (s saveDb) printStatistics(stat stats) {
-	// this is hard to read i know. I tried to made these readlable by separating them into new lines
-	schema := `INSERT INTO %s (type, message,
-		ip, hostname, port,
-        total_successful_probes, total_unsuccessful_probes,
-        last_successful_probe, last_unsuccessful_probe,
-        latency_min, latency_avg, latency_max,
-        start_timestamp, end_timestamp,
-        total_duration, total_uptime, total_downtime,
-		longest_uptime, longest_uptime_start, longest_uptime_end,
-     	longest_downtime, longest_downtime_start, longest_downtime_end,
-        total_packets,
-		timestamp)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+const (
+	typeStatistics     = "statistics"
+	typeHostnameChange = "hostname change"
+)
 
-	totalDuration := stat.totalUptime + stat.totalDowntime
+func (s saveDb) printStatistics(stat stats) {
+	// hostname_changes,
+	// hostname_changed_to,
+	// hostname_change_time,
+	schema := `INSERT INTO %s (type, timestamp,
+		addr, hostname, port, hostname_resolve_tries,
+		total_successful_probes, total_unsuccessful_probes,
+		last_successful_probe, last_unsuccessful_probe,
+		total_packets, total_packet_loss,
+		total_uptime, total_downtime,
+		longest_uptime, longest_uptime_end, longest_uptime_start,
+		longest_downtime, longest_downtime_start, longest_downtime_end,
+		latency_min, latency_avg, latency_max,
+		start_timestamp, end_timestamp, total_duration)
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+
+	totalPackets := stat.totalSuccessfulProbes + stat.totalUnsuccessfulProbes
+	packetLoss := (float32(stat.totalUnsuccessfulProbes) / float32(totalPackets)) * 100
+	if math.IsNaN(float64(packetLoss)) {
+		packetLoss = 0
+	}
+
 	s.saveToDb(schema,
-		statisticsEvent, fmt.Sprintf("stats for %s", stat.userInput.hostname),
-		stat.userInput.ip.String(), stat.userInput.hostname, stat.userInput.port,
+		typeStatistics, time.Now(),
+		stat.userInput.ip.String(), stat.userInput.hostname, stat.userInput.port, stat.retriedHostnameLookups,
 		stat.totalSuccessfulProbes, stat.totalUnsuccessfulProbes,
-		stat.lastSuccessfulProbe, stat.lastSuccessfulProbe,
-		stat.rttResults.min, stat.rttResults.average, stat.rttResults.max,
-		stat.startTime, stat.endTime,
-		totalDuration.String(),stat.totalUptime.String(), stat.totalDowntime.String(),
+		stat.lastSuccessfulProbe, stat.lastUnsuccessfulProbe,
+		totalPackets, packetLoss,
+		stat.totalUptime.String(), stat.totalDowntime.String(),
 		stat.longestUptime.duration.String(), stat.longestUptime.start, stat.longestUptime.end,
 		stat.longestDowntime.duration.String(), stat.longestDowntime.start, stat.longestDowntime.end,
-		stat.totalSuccessfulProbes+stat.totalUnsuccessfulProbes,
-		time.Now(),
+		stat.rttResults.min, stat.rttResults.average, stat.rttResults.max,
+		stat.startTime, stat.endTime, stat.endTime.Sub(stat.startTime).String(),
 	)
 
-	for _, host := range stat.hostnameChanges {
-		s.saveHostNameChange(host)
-	}
+	// for _, host := range stat.hostnameChanges {
+	// 	s.saveHostNameChange(host)
+	// }
 
 }
 
 func (s saveDb) printVersion() {
-	statement := "INSERT INTO %s (type, message, timestamp) VALUES (?, ?, ?, ?)"
-	s.saveToDb(statement, retrySuccessEvent, "TCPING version "+version, time.Now())
+	// statement := "INSERT INTO %s (type, message, timestamp) VALUES (?, ?, ?, ?)"
+	// s.saveToDb(statement, retrySuccessEvent, "TCPING version "+version, time.Now())
 }
 
 func (s saveDb) printInfo(format string, args ...any) {
-	statement := "INSERT INTO %s (type, message, timestamp) VALUES (?, ?, ?)"
-	s.saveToDb(statement, infoEvent, fmt.Sprintf(format, args...), time.Now())
+	// statement := "INSERT INTO %s (type, message, timestamp) VALUES (?, ?, ?)"
+	// s.saveToDb(statement, infoEvent, fmt.Sprintf(format, args...), time.Now())
 }
 
 func (s saveDb) printError(format string, args ...any) {
-	statement := "INSERT INTO %s (type, message, timestamp) VALUES (?, ?, ?)"
-	s.saveToDb(statement, errorEvent, fmt.Sprintf(format, args...), time.Now())
+	// statement := "INSERT INTO %s (type, message, timestamp) VALUES (?, ?, ?)"
+	// s.saveToDb(statement, errorEvent, fmt.Sprintf(format, args...), time.Now())
 }
--- db_test.go
+++ db_test.go
@@ -19,161 +19,11 @@ func TestNewDB(t *testing.T) {
 	defer rows.Close()
 	defer s.db.Close()
 
-	var tNames []string
-	for rows.Next() {
-		var tblName string
-		if err := rows.Scan(&tblName); err != nil {
-			t.Error(err)
-			return
-		}
-		// this is automatically created by SQLite
-		if tblName == "sqlite_sequence" {
-			continue
-		}
-		tNames = append(tNames, tblName)
-	}
-
-	if len(tNames) != 2 {
-		t.Errorf("expected 2 tables; got %d", len(tNames))
-		return
-	}
-
-	tableName, hostnameChangeTable := s.tableName, s.hostnameChangeTable
-	if tNames[0] != tableName {
-		t.Errorf("expected %s; got %s", tableName, tNames[0])
-	}
-
-	if tNames[1] != hostnameChangeTable {
-		t.Errorf("expected %s; got %s", hostnameChangeTable, tNames[1])
-	}
-}
-
-func TestDbHostnameSave(t *testing.T) {
-	arg := []string{"localhost", "8001"}
-	s := newDb(arg, ":memory:")
-
-	ipAddresses := []string{
-		"192.168.1.1",
-		"10.0.0.1",
-		"172.16.0.1",
-		"2001:0db8:85a3:0000:0000:8a2e:0370:7334",
-	}
-
-	var hostNames []hostnameChange
-	for _, ip := range ipAddresses {
-		host := hostnameChange{
-			Addr: netip.MustParseAddr(ip),
-			When: time.Now(),
-		}
-		hostNames = append(hostNames, host)
-	}
-
-	for _, host := range hostNames {
-		s.saveHostNameChange(host)
-	}
-
-	var dbIp []string
-
-	rows, err := s.db.Query("SELECT ip FROM " + s.hostnameChangeTable)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	for rows.Next() {
-		var ip string
-
-		err = rows.Scan(&ip)
-		if err != nil {
-			t.Error(err)
-			return
-		}
-		dbIp = append(dbIp, ip)
-	}
-
-	for i, ip := range hostNames {
-		if dbIp[i] != ip.Addr.String() {
-			t.Errorf("expected %q; got %q", ip.Addr.String(), dbIp[i])
-			return
-		}
-	}
-
-}
-
-func TestDbPrintStart(t *testing.T) {
-	arg := []string{"localhost", "8001"}
-	s := newDb(arg, ":memory:")
-
-	expectedHost := "localhost"
-	exptectedPort := uint16(8001)
-	s.printStart(expectedHost, exptectedPort)
-	rows, err := s.db.Query("SELECT hostname, port, timestamp FROM " + s.tableName)
-
-	if err != nil {
-		t.Log(err)
-		return
-	}
-
-	var hostname string
-	var port int
-	var timeStamp time.Time
-	for rows.Next() {
-		err = rows.Scan(&hostname, &port, &timeStamp)
-		if err != nil {
-			t.Error(err)
-			return
-		}
-	}
-
-	if expectedHost != hostname {
-		t.Errorf("expected %q; got %q", expectedHost, hostname)
-		return
-	} else if int(exptectedPort) != port {
-		t.Errorf("expected %q; got %q", exptectedPort, port)
-		return
-	} else if timeStamp.IsZero() {
-		t.Error("timeStamp is empty")
-	}
-}
-
-func TestDbPrintProbeSuccess(t *testing.T) {
-	arg := []string{"localhost", "8001"}
-	s := newDb(arg, ":memory:")
-
-	expectedHost := "localhost"
-	expectedIp := "127.0.0.1"
-	exptectedPort := uint16(8001)
-	expectedStreak := 100
-	expectedRtt := float32(1.0001)
-
-	s.printProbeSuccess(expectedHost, expectedIp, exptectedPort, uint(expectedStreak), expectedRtt)
-	rows, err := s.db.Query("SELECT total_successful_probes, latency, timestamp FROM " + s.tableName)
-
-	if err != nil {
-		t.Log(err)
-		return
-	}
-
-	var successfullProves int
-	var latency float32
-	var timeStamp time.Time
-	for rows.Next() {
-		err = rows.Scan(&successfullProves, &latency, &timeStamp)
-		if err != nil {
-			t.Error(err)
-			return
-		}
-	}
-
-	if successfullProves != expectedStreak {
-		t.Errorf("expected %q; got %q", expectedHost, successfullProves)
-		return
-	} else if expectedRtt != latency {
-		t.Errorf("expected %f; got %f", expectedRtt, latency)
-		return
-	} else if timeStamp.IsZero() {
-		t.Error("timeStamp is empty")
-	}
+	rows.Next()
+	var tblName string
+	err = rows.Scan(&tblName)
+	Nil(t, err)
+	Equals(t, tblName, s.tableName)
 }
 
 func TestDbPrintStatistics(t *testing.T) {
@@ -183,11 +33,12 @@ func TestDbPrintStatistics(t *testing.T) {
 
 	// There are many fields, so many things could go wrong; that's why this elaborate test.
 	stat := stats{
-		startTime:             time.Now(),
-		endTime:               time.Now().Add(10 * time.Minute),
-		lastSuccessfulProbe:   time.Now().Add(1 * time.Minute),
-		lastUnsuccessfulProbe: time.Now().Add(2 * time.Minute),
-		printer:               s,
+		startTime:              time.Now(),
+		endTime:                time.Now().Add(10 * time.Minute),
+		lastSuccessfulProbe:    time.Now().Add(1 * time.Minute),
+		lastUnsuccessfulProbe:  time.Now().Add(2 * time.Minute),
+		retriedHostnameLookups: 10,
+		printer:                s,
 		longestUptime: longestTime{
 			start:    time.Now().Add(20 * time.Second),
 			end:      time.Now().Add(80 * time.Second),
@@ -218,16 +69,16 @@ func TestDbPrintStatistics(t *testing.T) {
 
 	// prepare the query
 	query := `SELECT
-	type,
-	ip, hostname, port,
-    total_successful_probes, total_unsuccessful_probes,
-    last_successful_probe, last_unsuccessful_probe,
-    latency_min, latency_avg, latency_max,
-    start_timestamp, end_timestamp,
-    total_duration, total_uptime, total_downtime,
-	longest_uptime, longest_uptime_start, longest_uptime_end,
-    longest_downtime, longest_downtime_start, longest_downtime_end,
-    total_packets
+		type,
+		addr, hostname, port, hostname_resolve_tries,
+		total_successful_probes, total_unsuccessful_probes,
+		last_successful_probe, last_unsuccessful_probe,
+		total_packets, total_packet_loss,
+		total_uptime, total_downtime,
+		longest_uptime, longest_uptime_end, longest_uptime_start,
+		longest_downtime, longest_downtime_start, longest_downtime_end,
+		latency_min, latency_avg, latency_max,
+		start_timestamp, end_timestamp, total_duration
 	FROM ` + s.tableName
 
 	rows, err := s.db.Query(query)
@@ -239,37 +90,41 @@ func TestDbPrintStatistics(t *testing.T) {
 	}
 
 	var (
-		typeOf, ip, hostname, port                     string
+		typeOf, addr, hostname, port                   string
+		hostNameResolveTries                           uint
 		totalSuccessfulProbes, totalUnsuccessfulProbes uint
 		lastSuccessfulProbe, lastUnsuccessfulProbe     time.Time
-		lMin, lAvg, lMax                               float32
-		startTimestamp, endTimestamp                   time.Time
-		totalDuration, totalUptime, totalDowntime      string
+		totalPackets                                   uint
+		totalPacketsLoss                               float32
+		totalUptime, totalDowntime                     string
 		longestUptime                                  string
 		longestUptimeStart, longestUptimeEnd           time.Time
 		longestDowntime                                string
 		longestDowntimeStart, longestDowntimeEnd       time.Time
-		totalPackets                                   uint
+		lMin, lAvg, lMax                               float32
+		startTimestamp, endTimestamp                   time.Time
+		totalDuration                                  string
 	)
 
 	err = rows.Scan(
 		&typeOf,
-		&ip, &hostname, &port,
+		&addr, &hostname, &port, &hostNameResolveTries,
 		&totalSuccessfulProbes, &totalUnsuccessfulProbes,
 		&lastSuccessfulProbe, &lastUnsuccessfulProbe,
-		&lMin, &lAvg, &lMax,
-		&startTimestamp, &endTimestamp,
-		&totalDuration, &totalUptime, &totalDowntime,
+		&totalPackets, &totalPacketsLoss,
+		&totalUptime, &totalDowntime,
 		&longestUptime, &longestUptimeStart, &longestUptimeEnd,
 		&longestDowntime, &longestDowntimeStart, &longestDowntimeEnd,
-		&totalPackets,
+		&lMin, &lAvg, &lMax,
+		&startTimestamp, &endTimestamp, &totalDuration,
 	)
+
 	Nil(t, err)
 
 	const timeFormat = "2006-01-02 15:04:05.999999999 -0700 -07"
 
 	Equals(t, typeOf, string(statisticsEvent))
-	Equals(t, ip, stat.userInput.ip.String())
+	Equals(t, addr, stat.userInput.ip.String())
 	Equals(t, hostname, stat.userInput.hostname)
 	Equals(t, totalSuccessfulProbes, stat.totalSuccessfulProbes)
 	Equals(t, totalUnsuccessfulProbes, stat.totalUnsuccessfulProbes)
@@ -280,8 +135,8 @@ func TestDbPrintStatistics(t *testing.T) {
 	Equals(t, startTimestamp.Format(timeFormat), stat.startTime.Format(timeFormat))
 	Equals(t, endTimestamp.Format(timeFormat), stat.endTime.Format(timeFormat))
 
-	actualDuration := stat.totalUptime + stat.totalDowntime
-	Equals(t, totalDuration, actualDuration.String())
+	actualDuration := stat.endTime.Sub(stat.startTime).String()
+	Equals(t, totalDuration, actualDuration)
 	Equals(t, totalUptime, stat.totalUptime.String())
 	Equals(t, totalDowntime, stat.totalDowntime.String())
 	Equals(t, totalPackets, stat.totalSuccessfulProbes+stat.totalUnsuccessfulProbes)
@@ -296,10 +151,10 @@ func TestDbPrintStatistics(t *testing.T) {
 }
 
 // Equals compares two values
-func Equals[T comparable](t *testing.T, got, want T) {
+func Equals[T comparable](t *testing.T, value, want T) {
 	t.Helper()
-	if want != got {
-		t.Errorf("wanted %v; got %v", want, got)
+	if want != value {
+		t.Errorf("wanted %v; got %v", want, value)
 		t.FailNow()
 	}
 }
