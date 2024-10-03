30e1db755f64cb4b73e7c3e62c449a702ece576b
Basil Gray <basil.s.gray@gmail.com>
2024-10-03 17:51:04 +0900

    Added tests TestPrintProbeSuccess and TestPrintProbeFail

--- statsprinter_test.go
+++ statsprinter_test.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
 	"testing"
 	"time"
+
+	"github.com/gookit/color"
+	"github.com/stretchr/testify/assert"
 )
 
 // dummyPrinter is a fake test implementation
@@ -88,6 +95,172 @@ func TestDurationToString(t *testing.T) {
 			if got := durationToString(tt.duration); got != tt.want {
 				t.Errorf("calcTime() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestPrintProbeSuccess(t *testing.T) {
+	stats := createTestStats(t)
+	stats.userInput.hostname = "example.com"
+
+	streak := uint(5)
+	rtt := float32(15.123)
+
+	testCases := []struct {
+		name           string
+		showTimestamp  bool
+		useHostname    bool
+		expectedOutput string
+	}{
+		{
+			name:           "With hostname, no timestamp",
+			showTimestamp:  false,
+			useHostname:    true,
+			expectedOutput: "\x1b[92mReply from %s (%s) on port %d TCP_conn=%d time=%.3f ms\n\x1b[0m",
+		},
+		{
+			name:           "With hostname, with timestamp",
+			showTimestamp:  true,
+			useHostname:    true,
+			expectedOutput: "\x1b[92m%s Reply from %s (%s) on port %d TCP_conn=%d time=%.3f ms\n\x1b[0m",
+		},
+		{
+			name:           "Without hostname, with timestamp",
+			showTimestamp:  true,
+			useHostname:    false,
+			expectedOutput: "\x1b[92m%s Reply from %s on port %d TCP_conn=%d time=%.3f ms\n\x1b[0m",
+		},
+		{
+			name:           "Without hostname, no timestamp",
+			showTimestamp:  false,
+			useHostname:    false,
+			expectedOutput: "\x1b[92mReply from %s on port %d TCP_conn=%d time=%.3f ms\n\x1b[0m",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			pp := newPlanePrinter(&tc.showTimestamp)
+
+			read, write, _ := os.Pipe()
+			os.Stdout = write
+			color.SetOutput(write)
+
+			hostname := stats.userInput.hostname
+			if !tc.useHostname {
+				hostname = ""
+			}
+
+			pp.printProbeSuccess(hostname, stats.userInput.ip.String(), stats.userInput.port, streak, rtt)
+
+			write.Close()
+
+			var buf bytes.Buffer
+			if _, err := io.Copy(&buf, read); err != nil {
+				t.Fatalf("Failed to read from pipe: %v", err)
+			}
+
+			output := buf.String()
+
+			var expected string
+			if tc.showTimestamp {
+				timestamp := time.Now().Format("2006-01-02 15:04:05")
+				if tc.useHostname {
+					expected = fmt.Sprintf(tc.expectedOutput, timestamp, stats.userInput.hostname, stats.userInput.ip, stats.userInput.port, streak, rtt)
+				} else {
+					expected = fmt.Sprintf(tc.expectedOutput, timestamp, stats.userInput.ip, stats.userInput.port, streak, rtt)
+				}
+			} else {
+				if tc.useHostname {
+					expected = fmt.Sprintf(tc.expectedOutput, stats.userInput.hostname, stats.userInput.ip, stats.userInput.port, streak, rtt)
+				} else {
+					expected = fmt.Sprintf(tc.expectedOutput, stats.userInput.ip, stats.userInput.port, streak, rtt)
+				}
+			}
+
+			assert.Equal(t, expected, output)
+		})
+	}
+}
+
+func TestPrintProbeFail(t *testing.T) {
+	stats := createTestStats(t)
+	stats.userInput.hostname = "example.com"
+
+	streak := uint(5)
+
+	testCases := []struct {
+		name           string
+		showTimestamp  bool
+		useHostname    bool
+		expectedOutput string
+	}{
+		{
+			name:           "With hostname, no timestamp",
+			showTimestamp:  false,
+			useHostname:    true,
+			expectedOutput: "\x1b[31mNo reply from %s (%s) on port %d TCP_conn=%d\n\x1b[0m",
+		},
+		{
+			name:           "With hostname, with timestamp",
+			showTimestamp:  true,
+			useHostname:    true,
+			expectedOutput: "\x1b[31m%s No reply from %s (%s) on port %d TCP_conn=%d\n\x1b[0m",
+		},
+		{
+			name:           "Without hostname, with timestamp",
+			showTimestamp:  true,
+			useHostname:    false,
+			expectedOutput: "\x1b[31m%s No reply from %s on port %d TCP_conn=%d\n\x1b[0m",
+		},
+		{
+			name:           "Without hostname, no timestamp",
+			showTimestamp:  false,
+			useHostname:    false,
+			expectedOutput: "\x1b[31mNo reply from %s on port %d TCP_conn=%d\n\x1b[0m",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			pp := newPlanePrinter(&tc.showTimestamp)
+
+			read, write, _ := os.Pipe()
+			os.Stdout = write
+			color.SetOutput(write)
+
+			hostname := stats.userInput.hostname
+			if !tc.useHostname {
+				hostname = ""
+			}
+
+			pp.printProbeFail(hostname, stats.userInput.ip.String(), stats.userInput.port, streak)
+
+			write.Close()
+
+			var buf bytes.Buffer
+			if _, err := io.Copy(&buf, read); err != nil {
+				t.Fatalf("Failed to read from pipe: %v", err)
+			}
+
+			output := buf.String()
+
+			var expected string
+			if tc.showTimestamp {
+				timestamp := time.Now().Format("2006-01-02 15:04:05")
+				if tc.useHostname {
+					expected = fmt.Sprintf(tc.expectedOutput, timestamp, stats.userInput.hostname, stats.userInput.ip, stats.userInput.port, streak)
+				} else {
+					expected = fmt.Sprintf(tc.expectedOutput, timestamp, stats.userInput.ip, stats.userInput.port, streak)
+				}
+			} else {
+				if tc.useHostname {
+					expected = fmt.Sprintf(tc.expectedOutput, stats.userInput.hostname, stats.userInput.ip, stats.userInput.port, streak)
+				} else {
+					expected = fmt.Sprintf(tc.expectedOutput, stats.userInput.ip, stats.userInput.port, streak)
+				}
+			}
+			assert.Equal(t, expected, output)
 		})
 	}
 }
