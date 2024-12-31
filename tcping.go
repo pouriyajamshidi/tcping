5db74a6695201106296674c8e65ac215bc9b2917
pouriyajamshidi <pouriya.jamshidi@gmail.com>
2024-12-31 11:48:08 +0100

    add a newline in printer selection logic

--- tcping.go
+++ tcping.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"syscall"
 	"time"
+
 	"github.com/google/go-github/v45/github"
 )
 
@@ -239,6 +240,7 @@ func setPrinter(tcping *tcping, outputJSON, prettyJSON *bool, noColor *bool, tim
 		colorRed("--pretty has no effect without the -j flag.")
 		usage()
 	}
+
 	if *outputJSON {
 		tcping.printer = newJSONPrinter(*prettyJSON)
 	} else if *outputDb != "" {
