a6a6c18db665988073c0a5a5e1650b3aebdda09e
steven@zarl.dev <steven@zarl.dev>
2025-12-16 14:09:15 +0000

    latest

--- cmd/main.go
+++ cmd/main.go
@@ -1,62 +1,11 @@
 package main
 
 import (
-	"context"
-	"net/netip"
+	"os"
 
-	"github.com/pouriyajamshidi/tcping/v3"
-	"github.com/pouriyajamshidi/tcping/v3/pingers"
-	"github.com/pouriyajamshidi/tcping/v3/printers"
+	"github.com/pouriyajamshidi/tcping/v3/internal/app"
 )
 
 func main() {
-	ip, err := netip.ParseAddr("161.35.175.61")
-	if err != nil {
-		tcping.HandleExit(err)
-	}
-	port := uint16(80)
-	prober := tcping.NewProber(pingers.NewTCPPinger(ip, port))
-	stats, err := prober.Probe(context.Background())
-	if err != nil {
-		tcping.HandleExit(err)
-	}
-	printer := printers.NewColorPrinter()
-	printer.PrintStatistics(&stats)
+	os.Exit(app.Run())
 }
-
-// tcping := &tcping.Result{}
-// stats := &statistics.Statistics{}
-
-// printer := input.ProcessUserInput(tcping, stats)
-
-// printer.PrintStart(stats)
-
-// tcping.StartTime = time.Now()
-
-// stats.IP = dns.ResolveHostname(printer, stats, true, false)
-
-// tcping.Ticker = time.NewTicker(tcping.Settings.IntervalBetweenProbes)
-// defer tcping.Ticker.Stop()
-
-// printers.SignalHandler(printer, stats)
-
-// if !tcping.Settings.NonInteractive {
-// 	go monitorStatsRequest(printer, stats)
-// }
-
-// var probeCount uint
-
-// for {
-// 	if tcping.Settings.ShouldRetryResolve {
-// 		dns.RetryResolveHostname(printer, stats, 300, true, false)
-// 	}
-
-// 	probes.Ping(stats, printer, tcping)
-
-// 	if tcping.Settings.ProbesBeforeQuit != 0 {
-// 		probeCount++
-// 		if probeCount == tcping.Settings.ProbesBeforeQuit {
-// 			printer.Shutdown(stats)
-// 		}
-// 	}
-// }
