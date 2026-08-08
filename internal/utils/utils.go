02ba1f556e88c479aaafdadf1cdd2f4245bbac8e
pouriyajamshidi <pouriya.jamshidi@gmail.com>
2026-08-08 10:56:22 +0200

    extract flag helper functions to config package

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,50 +1,12 @@
 package utils
 
 import (
-	"context"
-	"encoding/json"
-	"flag"
 	"fmt"
 	"math"
-	"net/http"
-	"os"
-	"regexp"
-	"strconv"
-	"strings"
 	"time"
 
-	"github.com/pouriyajamshidi/tcping/v3/internal/consts"
 	"github.com/pouriyajamshidi/tcping/v3/internal/models"
 )
-
-// Usage prints how tcping should be run
-func Usage() {
-	executableName := os.Args[0]
-
-	fmt.Printf("\nTCPING version %s\n\n", consts.Version)
-	fmt.Printf("Try running %s like:\n", executableName)
-	fmt.Printf("%s www.example.com 443\n", executableName)
-	fmt.Println("Or use the <hostname/ip:port> format:")
-	fmt.Printf("%s www.example.com:443\n", executableName)
-	fmt.Printf("\n[optional flags]\n")
-
-	flag.VisitAll(func(f *flag.Flag) {
-		flagName := f.Name
-		if len(f.Name) > 1 {
-			flagName = "-" + flagName
-		}
-
-		fmt.Printf("  -%s : %s\n", flagName, f.Usage)
-	})
-
-	os.Exit(1)
-}
-
-// ShowVersion displays the version and exits
-func ShowVersion() {
-	fmt.Printf("TCPING version %s\n", consts.Version)
-	os.Exit(0)
-}
 
 // NanoToMillisecond returns an amount of milliseconds from nanoseconds.
 // Using duration.Milliseconds() is not an option, because it drops
@@ -124,98 +86,4 @@ func SetLongestDuration(start time.Time, duration time.Duration, longest *models
 	if longest.End.IsZero() || newLongest.Duration >= longest.Duration {
 		*longest = newLongest
 	}
-}
-
-// compareVersions is used to compare tcping versions
-func compareVersions(v1, v2 string) int {
-	parts1 := strings.Split(v1, ".")
-	parts2 := strings.Split(v2, ".")
-
-	for i := 0; i < len(parts1) && i < len(parts2); i++ {
-		n1, _ := strconv.Atoi(parts1[i])
-		n2, _ := strconv.Atoi(parts2[i])
-
-		if n1 < n2 {
-			return -1
-		}
-
-		if n1 > n2 {
-			return 1
-		}
-	}
-
-	// for cases in which version numbers differ in length
-	if len(parts1) < len(parts2) {
-		return -1
-	}
-
-	if len(parts1) > len(parts2) {
-		return 1
-	}
-
-	return 0
-}
-
-// CheckForUpdates checks for newer versions of tcping
-func CheckForUpdates() {
-	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/releases/latest", consts.Owner, consts.Repo)
-
-	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, nil)
-	if err != nil {
-		fmt.Printf("Could not create request: %s", err)
-		os.Exit(1)
-	}
-
-	client := &http.Client{Timeout: 5 * time.Second}
-
-	// optional (GitHub recommends)
-	req.Header.Set("User-Agent", "pouriyajamshidi-tcping-update-check")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("Failed to check for updates %s", err)
-		os.Exit(1)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Failed to check for updates: HTTP %d", resp.StatusCode)
-		os.Exit(1)
-	}
-
-	release := struct {
-		TagName string `json:"tag_name"`
-	}{}
-
-	if err := json.NewDecoder(resp.Body).Decode(&release); err != nil {
-		fmt.Printf("Failed to parse release info: %s", err)
-		os.Exit(1)
-	}
-
-	reg := `^v?(\d+\.\d+\.\d+)$`
-	latestTagName := release.TagName
-	re := regexp.MustCompile(reg)
-	m := re.FindStringSubmatch(latestTagName)
-	if len(m) == 0 {
-		fmt.Printf("Failed to check for updates. The version name does not match the rule: %s", latestTagName)
-		os.Exit(1)
-	}
-
-	latestVer := m[1]
-
-	comparison := compareVersions(consts.Version, latestVer)
-
-	if comparison < 0 {
-		fmt.Printf("Found newer version %s", latestVer)
-		fmt.Printf("Please update TCPING from the URL below:")
-		fmt.Printf("https://github.com/%s/%s/releases/tag/%s",
-			consts.Owner, consts.Repo, latestTagName)
-	} else if comparison > 0 {
-		fmt.Printf("Current version %s is newer than the latest release %s",
-			consts.Version, latestVer)
-	} else {
-		fmt.Printf("You have the latest version: %s", consts.Version)
-	}
-
-	os.Exit(0)
 }
