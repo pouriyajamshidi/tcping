98f948a5a840b17aec5e06279f5e2d7c0c57ce6d
pouriyajamshidi <pouriya.jamshidi@gmail.com>
2026-08-24 16:13:02 +0200

    remove the redundant finalizeStatistics function

--- internal/probers/prober.go
+++ internal/probers/prober.go
@@ -217,21 +217,3 @@ func (p *Prober) finalizeStatistics() {
 		utils.SetLongestDuration(p.Statistics.StartOfUptime, upDuration, &p.Statistics.LongestUp)
 	}
 }
-
-func finalizeStatistics(s *stats.Statistics) {
-	s.EndTime = time.Now()
-	s.UpTime = s.EndTime.Sub(s.StartTime)
-
-	if s.LastProbeHadFailed {
-		downDuration := s.EndTime.Sub(s.StartOfDowntime)
-		s.TotalDowntime += downDuration
-		utils.SetLongestDuration(s.StartOfDowntime, downDuration, &s.LongestDown)
-		return
-	}
-
-	if !s.StartOfUptime.IsZero() {
-		upDuration := s.EndTime.Sub(s.StartOfUptime)
-		s.TotalUptime += upDuration
-		utils.SetLongestDuration(s.StartOfUptime, upDuration, &s.LongestUp)
-	}
-}
