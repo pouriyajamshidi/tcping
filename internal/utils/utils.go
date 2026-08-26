a5ff331e9ace3a8b428b4d04525b29a56db921ba
pouriyajamshidi <pouriya.jamshidi@gmail.com>
2026-08-26 22:33:06 +0200

    use a simpler logic to convert the duration to string

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"fmt"
-	"math"
 	"time"
 )
 
@@ -32,40 +31,41 @@ func MaxDuration(x, y time.Duration) time.Duration {
 }
 
 // DurationToString creates a human-readable string for a given duration
-func DurationToString(duration time.Duration) string {
-	hours := math.Floor(duration.Hours())
-	if hours > 0 {
-		duration -= time.Duration(hours * float64(time.Hour))
-	}
+func DurationToString(d time.Duration) string {
+	hours := d / time.Hour
+	d %= time.Hour
 
-	minutes := math.Floor(duration.Minutes())
-	if minutes > 0 {
-		duration -= time.Duration(minutes * float64(time.Minute))
-	}
+	minutes := d / time.Minute
+	d %= time.Minute
 
-	seconds := duration.Seconds()
+	seconds := d.Seconds()
 
 	switch {
-	// Hours
 	case hours >= 2:
-		return fmt.Sprintf("%.0f hours %.0f minutes %.0f seconds", hours, minutes, seconds)
-	case hours == 1 && minutes == 0 && seconds == 0:
-		return fmt.Sprintf("%.0f hour", hours)
+		return fmt.Sprintf("%d hours %d minutes %.0f seconds", hours, minutes, seconds)
+
 	case hours == 1:
-		return fmt.Sprintf("%.0f hour %.0f minutes %.0f seconds", hours, minutes, seconds)
+		if minutes == 0 && seconds == 0 {
+			return "1 hour"
+		}
+		return fmt.Sprintf("1 hour %d minutes %.0f seconds", minutes, seconds)
 
-	// Minutes
 	case minutes >= 2:
-		return fmt.Sprintf("%.0f minutes %.0f seconds", minutes, seconds)
-	case minutes == 1 && seconds == 0:
-		return fmt.Sprintf("%.0f minute", minutes)
-	case minutes == 1:
-		return fmt.Sprintf("%.0f minute %.0f seconds", minutes, seconds)
+		return fmt.Sprintf("%d minutes %.0f seconds", minutes, seconds)
 
-	// Seconds
-	case seconds == 0 || seconds == 1 || seconds >= 1 && seconds < 1.1:
-		return fmt.Sprintf("%.0f second", seconds)
-	case seconds < 1:
+	case minutes == 1:
+		if seconds == 0 {
+			return "1 minute"
+		}
+		return fmt.Sprintf("1 minute %.0f seconds", seconds)
+
+	case seconds == 0:
+		return "0 seconds"
+
+	case seconds < 1.1:
+		return "1 second"
+
+	case seconds < 2:
 		return fmt.Sprintf("%.1f seconds", seconds)
 
 	default:
