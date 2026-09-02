d3c54fa6be096dbfa758b0fbef26236a16ec1138
pouriyajamshidi <pouriya.jamshidi@gmail.com>
2026-09-02 13:52:36 +0200

    add some new TODOs

--- cmd/tcping.go
+++ cmd/tcping.go
@@ -33,6 +33,8 @@ Library support, so other codebases can import tcping (in priority order):
    several packages to be wired together by hand
 6. Treat exported types/fields/methods as a public API contract once this
    is importable (semver discipline)
+7. Sending over proxy connections
+8. mdev
 */
 
 func main() {
